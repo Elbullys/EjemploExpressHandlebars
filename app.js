b507c7a8bac4ad70ff87724bc8f7e1090e5a5d3b
Néstor León <ness_u2@hotmail.com>
2022-05-03 15:38:40 -0500

    se explicó sintaxis de handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const req = require('express/lib/request');
 const path = require('path');
 
 const app = express();
@@ -27,8 +28,45 @@ app.get('/', (req, res) => {
 
 app.get('/productos', (req, res) => {
     // Renderizar vista home
-    res.render('Productos/homeProductos', {layout: 'main'});
+    res.render('Productos/homeProductos', 
+        {
+            layout: 'main',
+            nombre: 'Néstor León',
+            persona: {
+                nombre: "Néstor León",
+                edad: 36
+            },
+            edad: 36,
+            bandera: false,
+            productos: [
+                {id: 1, descripcion: "Leche", costo: 12.50},
+                {id: 2, descripcion: "Cerveza", costo: 22.50},
+                {id: 3, descripcion: "Tequila", costo: 90.50},
+                {id: 4, descripcion: "Vodka", costo: 122.50},
+                {id: 5, descripcion: "Ron", costo: 222.50},
+            ],
+        });
 });
+
+app.get('/productos/:id', (req, res) => {
+    res.render('Productos/detalleProducto', {
+        layout: 'main',
+        id: req.params['id']
+    });
+});
+
+app.get('/productos/edit/:id', (req, res) => {
+    res.render('Productos/editProducto', {
+        layout: 'main',
+        id: req.params['id'],
+        producto: {
+            id: req.params['id'], 
+            descripcion: "Cerveza", 
+            costo: 22.50
+        }    
+    });
+});
+
 
 //========================================================
 //      LISTEN
